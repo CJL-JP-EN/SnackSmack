f79961435aa10322d6d5ec2f142fed604177b7d9
CJL-JP-EN <lambertcj03@gmail.com>
2025-10-26 21:36:47 -0700

    Updated the calendarScreen.kt so that you can now make events. It will also locally store said events so that if you navigate the various pages it will retain the information.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.snacksmack"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -65,4 +65,5 @@ dependencies {
     implementation(libs.firebase.analytics)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation("com.google.code.gson:gson:2.13.2")
 }
