e473c404f00ebf33d955d413223781efa277bc1e
CJL-JP-EN <lambertcj03@gmail.com>
2025-10-28 17:04:36 -0700

    updated the custom icons for the app logo as well as added it onto the home screen, updated the colors, updated small bugs i had on my end from pull issues

--- app/src/main/java/com/example/snacksmack/MainActivity.kt
+++ app/src/main/java/com/example/snacksmack/MainActivity.kt
@@ -6,39 +6,35 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.snacksmack.ui.theme.SnackSmackTheme
-import com.example.snacksmack.waterTrackingScreen   // capital w
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            SnackSmackTheme { MyApp() }
-        }
-    }
-}
-
-@Composable
-fun MyApp() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { NavigationButtons(navController = navController) }
-    ) { innerPadding ->
-        NavHost(
-            navController = navController,
-            startDestination = "home",
-            modifier = Modifier.padding(innerPadding)
-        ) {
-            composable("home")         { HomeScreen() }
-            composable("waterTracking"){ waterTrackingScreen() }
-            composable("calendar")     { CalendarScreen() }
-            composable("profile")      { ProfileScreen() }
+            SnackSmackTheme {
+                val navController = rememberNavController()
+                Scaffold(
+                    bottomBar = { NavigationButtons(navController = navController) }
+                ) { innerPadding ->
+                    NavHost(
+                        navController = navController,
+                        startDestination = "home",
+                        modifier = Modifier.padding(innerPadding)
+                    ) {
+                        composable("home") { HomeScreen() }
+                        composable("profile") { ProfileScreen() }
+                        composable("waterTracking") { waterTrackingScreen() }
+                        composable("calendar") { CalendarScreen() }
+                    }
+                }
+            }
         }
     }
 }
